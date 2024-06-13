1604fe4bbc3ff1a8082808aed80d2f57871814bc
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-13 19:17:02 +0530

    heading txt

--- src/Components/Homepage/HomeHeadingVid/HomeHeadingVid.jsx
+++ src/Components/Homepage/HomeHeadingVid/HomeHeadingVid.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import banner from "./../../../Media/banner.mp4";
 import "./HomeHeadingVid.css";
+import HomeHeadingVidTxt from "./HomeHeadingVidTxt";
 const HomeHeadingVid = () => {
   return (
     <div className="homeHeadingContainer">
       <video src={banner} autoPlay loop muted />
+      <HomeHeadingVidTxt/>
     </div>
   );
 };
---
+++ src/Components/Homepage/HomeHeadingVid/HomeHeadingVidTxt.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeHeadingVid.css";
+const HomeHeadingVidTxt = () => {
+  return (
+    <div className="homeHeadingTxtContainer">
+      <div className="homeHeadingCover">
+        <h2>
+          Start Your German Language Learning Experience Now with{" "}
+          <span> Prizma Academy's </span> Online Courses!
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default HomeHeadingVidTxt;
